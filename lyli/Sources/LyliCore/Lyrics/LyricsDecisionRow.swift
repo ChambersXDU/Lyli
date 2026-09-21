479c1a32255c4aa6c8be1a271e01ee9a6c90ad90
ChambersXDU <78330120+ChambersXDU@users.noreply.github.com>
2026-09-21 15:17:07 +0800

    refactor: remove lyrics decision explanation

--- lyli/Sources/LyliCore/Lyrics/LyricsDecisionRow.swift
+++
@@ -1,8 +0,0 @@
-import Foundation
-
-public enum LyricsDecisionRow {
-
-    public static func isInstrumentalMarker(instrumental: Bool?, score: Int) -> Bool {
-        instrumental == true && score < 0
-    }
-}
